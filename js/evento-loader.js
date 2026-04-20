15e157d890d5498ecbc2d8956ac1e9d20cc331e0
Arturo Cruz <foro7.arturocruz@gmail.com>
2026-04-20 03:23:24 -0600

    Rediseño completo invitación: tema rojo/oro, todas las secciones con datos Supabase, UX mejorada

--- js/evento-loader.js
+++ js/evento-loader.js
@@ -19,7 +19,7 @@
     // Recepción
     { seccion: 'recepcion', key: 'lugar',     sel: '[data-campo="recepcion-lugar"]' },
     { seccion: 'recepcion', key: 'direccion', sel: '[data-campo="recepcion-direccion"]' },
-    { seccion: 'recepcion', key: 'hora',      sel: '[data-campo="recepcion-hora"]' },
+    { seccion: 'recepcion', key: 'hora_fin',  sel: '[data-campo="recepcion-hora"]' },
     { seccion: 'recepcion', key: 'maps_url',  sel: '[data-campo="recepcion-maps"]', attr: 'href' },
     // Protagonistas
     { seccion: 'protagonistas', key: 'nombre',       sel: '[data-campo="protagonista-nombre"]' },
@@ -31,6 +31,30 @@
     { seccion: 'vestimenta', key: 'colores_reservados', sel: '[data-campo="vestimenta-colores"]' },
     // Invitación web
     { seccion: 'invitacion_web', key: 'frase', sel: '[data-campo="invitacion-frase"]' },
+    // Padrinos de honor
+    { seccion: 'padrinos_honor', key: 'madrina', sel: '[data-campo="madrina-nombre"]' },
+    { seccion: 'padrinos_honor', key: 'padrino', sel: '[data-campo="padrino-nombre"]' },
+    // Madrinas
+    { seccion: 'madrinas', key: 'ramo',          sel: '[data-campo="madrina-ramo"]' },
+    { seccion: 'madrinas', key: 'unas',          sel: '[data-campo="madrina-unas"]' },
+    { seccion: 'madrinas', key: 'vals',          sel: '[data-campo="madrina-vals"]' },
+    { seccion: 'madrinas', key: 'collar',        sel: '[data-campo="madrina-collar"]' },
+    { seccion: 'madrinas', key: 'corona',        sel: '[data-campo="madrina-corona"]' },
+    { seccion: 'madrinas', key: 'pastel',        sel: '[data-campo="madrina-pastel"]' },
+    { seccion: 'madrinas', key: 'brindis',       sel: '[data-campo="madrina-brindis"]' },
+    { seccion: 'madrinas', key: 'peinado',       sel: '[data-campo="madrina-peinado"]' },
+    { seccion: 'madrinas', key: 'rosario',       sel: '[data-campo="madrina-rosario"]' },
+    { seccion: 'madrinas', key: 'vestido',       sel: '[data-campo="madrina-vestido"]' },
+    { seccion: 'madrinas', key: 'musica_dj',     sel: '[data-campo="madrina-musica-dj"]' },
+    { seccion: 'madrinas', key: 'recuerdos',     sel: '[data-campo="madrina-recuerdos"]' },
+    { seccion: 'madrinas', key: 'decoracion',    sel: '[data-campo="madrina-decoracion"]' },
+    { seccion: 'madrinas', key: 'foto_video',    sel: '[data-campo="madrina-foto-video"]' },
+    { seccion: 'madrinas', key: 'maquillaje',    sel: '[data-campo="madrina-maquillaje"]' },
+    { seccion: 'madrinas', key: 'zapatillas',    sel: '[data-campo="madrina-zapatillas"]' },
+    { seccion: 'madrinas', key: 'biblia_misal',  sel: '[data-campo="madrina-biblia-misal"]' },
+    { seccion: 'madrinas', key: 'centros_mesa',  sel: '[data-campo="madrina-centros-mesa"]' },
+    { seccion: 'madrinas', key: 'invitaciones',  sel: '[data-campo="madrina-invitaciones"]' },
+    { seccion: 'madrinas', key: 'ultima_muneca', sel: '[data-campo="madrina-ultima-muneca"]' },
   ];
 
   async function load(slug) {
